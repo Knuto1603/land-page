667307424868aa5cbea868a53cb048a0183f6a50
Knuto <sergiofernando_1603@hotmail.com>
2024-11-24 16:13:15 -0500

    Segundo Comit

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    hola
     <App />
   </React.StrictMode>
 );
